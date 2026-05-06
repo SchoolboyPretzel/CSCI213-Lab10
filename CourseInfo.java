f92d2eaf4deb686709f23bb70f2fda2df8b0636f
Roland Pelzel <rolandpelzel@go.rmc.edu>
2026-05-06 14:05:13 -0400

    Updated

--- CourseInfo.java
+++ CourseInfo.java
@@ -8,11 +8,15 @@ public class CourseInfo {
 		String semester = "Spring 2026";
 		int credits = 4;
 		String location = "COP 204";
+		String time = "13:10-14:40";
+		int numStudents = 20;
 		
 		System.out.println("Course Name: " + course);
 		System.out.println("Instructor: " + instructor);
 		System.out.println("Semester: " + semester);
 		System.out.println("Credits: " + credits);
 		System.out.println("Classroom: " + location);
+		System.out.println("Class Time: " + time);
+		System.out.println("Number of Students: " + numStudents);
 	}
 }
