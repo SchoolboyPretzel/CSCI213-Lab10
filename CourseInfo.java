89afd5c2af1be7751ce064cc777c1374de55336f
Roland Pelzel <rolandpelzel@go.rmc.edu>
2026-05-06 13:51:47 -0400

    Initial version of CourseInfo

---
+++ CourseInfo.java
@@ -0,0 +1,18 @@
+
+
+public class CourseInfo {
+	
+	public static void main(String[] args) {
+		String course = "Object-Oriented Programming";
+		String instructor = "Dr. Elouni";
+		String semester = "Spring 2026";
+		int credits = 4;
+		String location = "COP 204";
+		
+		System.out.println("Course Name: " + course);
+		System.out.println("Instructor: " + instructor);
+		System.out.println("Semester: " + semester);
+		System.out.println("Credits: " + credits);
+		System.out.println("Classroom: " + location);
+	}
+}
